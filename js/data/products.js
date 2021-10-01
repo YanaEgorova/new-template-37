56e94af669a9a1195dcb8647f6b67f9eb56770e2
Yana <yanaegorova69.3@gmail.com>
2021-10-01 21:34:49 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,168 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Facial Razor',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Provides easy removal of peach fuzz and gentle exfoliation of dull, dead skin.',
+            },
+            {
+                text: 'For optimal performance, replace razor blades every 2 - 3 months or as needed; 3 replacement blades included.',
+            },
+            {
+                text: 'Stainless steel blades are sanitary, durable and easy to clean.',
+            },
+            {
+                text: 'HOW TO USE:1- Start with clean and moisturized skin OR use while washing your face, with your face wash still on the skin. 2- After removing cap from tool, hold the razor at an angle and apply short, feathery strokes downward and with light pressure anywhere you need to remove excess hair. 3- Once finished, rinse face and apply moisturizer or serum to avoid drying. 4- Rinse razor with warm water or an alcohol wipe to clean. Once dry, replace cap and store.',
+            },
+            {
+                text: 'TOOL CARE: Clean with alcohol to prevent infection and bacteria build-up before and after each use. Place the cap back on the razors after use and store in a dry place. Handle razor with care, blade is sharp. PRODUCT DIMENSIONS - 10 properties.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Vase',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Perfect for small plants.',
+            },
+            {
+                text: 'Neutral matte beige finish.',
+            },
+            {
+                text: 'Has handles on sides.',
+            },
+            {
+                text: 'Perfect for a modern home.',
+            },
+            {
+                text: '10"L x 7"W x 11-1/2"H.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png', './img/img2-4.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Toaster',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Dual control panels make this two toasters in one; smooth brushed stainless housing with polished black chrome and black accents.',
+            },
+            {
+                text: 'Custom control: two 6-setting browning dials, dual reheat, defrost and bagel buttons with LED indicators.',
+            },
+            {
+                text: '1-1/2 inch - wide toasting slots, extra-lift carriage lever, slide-out crumb tray and convenient cord storage.',
+            },
+            {
+                text: 'Instruction book included.',
+            },
+            {
+                text: 'Actual Unit Dimensions: 11.15" L x10.65" W x7.5" H.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Waterpik',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                title: 'Whiter Teeth, Healthier Gums',
+                text: 'The Waterpik  provides all the clinically proven benefits of water flossing plus extra teeth whitening, restores your teeth\'s natural whiteness in just 4 weeks while you floss.',
+            },
+            {
+                title: 'Remove Stains',
+                text: 'Removes up to 25 percent more stains than brushing alone, removes up to 99.9 percent of plaque from treated areas, and is up to 50 percent more effective for improving gum health than dental floss.',
+            },
+            {
+                title: '30 Whitening Tablets',
+                text: 'As gentle as regular toothpaste, use one fresh mint whitening tablet daily. Place in the infuser built into the flosser handle to remove stains from hard to reach areas, between teeth, and at the gum line.',
+            },
+            {
+                title: 'Whiten Teeth At Home',
+                text: 'The Waterpik  is ideal for those who want to easily whiten teeth at home without harsh bleaching chemicals or expensive teeth whitening treatments.',
+            },
+            {
+                title: 'Included',
+                text: '30 whitening tablets, 2 classic whitening tips (WJT 100), 1 plaque seeker whitening tip (WPS 100), 1 orthodontic whitening tip (WOD 100), tip storage case. Pressure range 10 to 100 psi (0.703 to 7.030 kilo gram/cm2). Flow rate per minute 13 ounces (400 millimeter). Reservoir capacity of 90 plus seconds.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Wireless Earbuds',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                title: 'Battery Life',
+                text: 'Up to 12 hours of listening time.',
+            },
+            {
+                title: 'Customized Fit',
+                text: 'All-day comfort with Flex-Form cable and four eartip options.',
+            },
+            {
+                title: 'The Music Stops When You Do',
+                text: 'Magnetic earbuds with Auto-Play/Pause.',
+            },
+            {
+                title: 'Extended Wireless Range',
+                text: 'Class 1 Bluetooth for extended range and fewer dropouts.',
+            },
+            {
+                title: 'On-Device Controls',
+                text: 'On-device controls for music, calls, and voice assistant.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Smart Watch',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Get ready to crush your wellness goals with body readings right on your wrist.',
+            },
+            {
+                title: 'Better Sleep Starts Here',
+                text: 'Wake up feeling refreshed and recharged with advanced sleep tracking; When you go to bed, your sleep tracker starts monitoring your sleep and SpO2 levels continuously.',
+            },
+            {
+                title: 'Be Smart About Your Heart',
+                text: 'Take care of your heart with accurate ECG monitoring and keep an eye on possible atrial fibrillation, a common form of irregular heart rhythm.',
+            },
+            {
+                title: 'Make Every Workout Count',
+                text: 'Get the most out of every exercise session with advanced workout tracking that recognizes 6 popular activities, from running to rowing to swimming, automatically in just 3 minutes; Stay motivated by connecting to live coaching sessions via your smartphone or to dynamic Group Challenges with your friends.',
+            },
+            {
+                title: 'Go the Extra Mile',
+                text: 'Improve your runs with advanced running coaching technology; VO2 Max readings assess your oxygen levels to manage and track your heart and lung endurance.',
+            },
+            {
+                title: 'Do More Right From Your Wrist',
+                text: 'Watch works as an extension of your phone so you can talk, stream, and text from your wrist on the go.',
+            },
+            {
+                title: 'Timeless Design, Elevated Style',
+                text: 'Show of your style with a luxurious and timeless watch design that’s great for any occasion; Personalize your look even more with a range of stylish watch faces and bands.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png'],
+    },
+]
